946795a1f2f5f33b886c79540c3ae8cb6d0bd6d8
xiaoquanquanxin <xxkl123@126.com>
2020-03-06 15:55:40 +0800

    整体逻辑get

--- js/housingDetail.js
+++ js/housingDetail.js
@@ -2,7 +2,35 @@ window.onload = function () {
     ;(function () {
         _eventUpwardTriangleButtonClick();
         _eventSelectDate();
+        _assignmentDate();
 
 
+        //  fixme   正式
+        // $.getJSON(APIList.salesSummaryV2, function (result) {
+        //     console.log(result);
+        //     if(result.isSuccess){
+        //         requestCallback(result.data);
+        //     }
+        // });
+        //  fixme   调试
+        requestCallback(mainData);
+        function requestCallback(data) {
+            console.log(data);
+            // console.log(data.yesterdayAndTotalSale);
+            //  转化格式
+            formattingTotalData(data.yesterdayAndTotalSale);
+            formattingYesterdayData(data.yesterdayAndTotalSale);
+            Object.keys(data.mapVillage).forEach(function (item) {
+                formattingTotalData(data.mapVillage[item]);
+                formattingYesterdayData(data.mapVillage[item]);
+            });
+            window.requestData = data;
+
+            //  哪一个楼盘
+            const village = decodeURIComponent(getQueryVariable('village'));
+            _assignmentRealEstateOnSale(data.mapVillage[village], 1);
+            //  默认绘制扇形统计图
+            pieDiagram1Fn(-1);
+        }
     }());
 };
